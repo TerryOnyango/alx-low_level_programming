06f8d66b373bf2156476450db757f4357c85e520
TerryOnyango <terryonyango001@gmail.com>
2022-07-31 05:50:42 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints name of program
+ * @argc: argument count
+ * @argv: array of pointers to arguement strings
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+argc = argc;
+printf("%s\n", *argv);
+return (0);
+}
